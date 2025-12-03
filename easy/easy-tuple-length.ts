01a2badb33a7042114d356b47be3e10e119aafd4
yunajoe <yunajoe@gmail.com>
2025-12-03 12:14:05 +0900

    easy-tuple-length

---
+++ easy/easy-tuple-length.ts
@@ -0,0 +1,25 @@
+import { Equal, Expect } from "./../utils";
+// ============= Test Cases =============
+
+const tesla = ["tesla", "model 3", "model X", "model Y"] as const;
+
+// readonly ["tesla", "model 3", "model X", "model Y"]
+
+const spaceX = [
+  "FALCON 9",
+  "FALCON HEAVY",
+  "DRAGON",
+  "STARSHIP",
+  "HUMAN SPACEFLIGHT",
+] as const;
+
+type cases = [
+  Expect<Equal<Length<typeof tesla>, 4>>,
+  Expect<Equal<Length<typeof spaceX>, 5>>,
+  // @ts-expect-error
+  Length<5>,
+  // @ts-expect-error
+  Length<"hello world">
+];
+
+type Length<T extends readonly any[]> = T["length"];
